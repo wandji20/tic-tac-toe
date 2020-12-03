72f5e7235c87b72304699008e4ec93c9423d52c7
wandji20 <wbertrand10@gmail.com>
2020-12-03 08:51:17 +0100

    modify board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,16 +1,17 @@
+require_relative './player.rb'
+
 class Board
-  def initalize()
-   
+  def initialize(player1, player2)
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    @player1 = player1
+    @player2 = player2
   end
-  def board_display()
-    @board_init = ["2 ", " ", " ", " ", " ", " ", " ", " ", " "]
-    puts " #{@board_init[0]} | #{@board_init[1]} | #{@board_init[2]} "
-    puts "-----------"
-    puts " #{@board_init[3]} | #{@board_init[4]} | #{@board_init[5]} "
-    puts "-----------"
-    puts " #{@board_init[6]} | #{@board_init[7]} | #{@board_init[8]} "
+
+  def display_board
+    puts "  #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts ' ---+---+---'
+    puts "  #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts ' ---+---+--- '
+    puts "  #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 end
-
-p my_board = Board.new
-p my_board.board_display()
