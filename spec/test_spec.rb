6fc45b598e377809a97447898a942553137a8b13
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-12-09 12:48:08 +0200

    final tested

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -3,16 +3,15 @@ require_relative '../lib/game'
 require_relative '../lib/board'
 
 describe Player do
- 
-  subject { Player.new("#{name}")}
+  subject { Player.new(name.to_s) }
   describe '#valid_name?' do
-  let (:name) {'wandji'}
+    let(:name) { 'wandji' }
     it 'returns true if name is valid' do
       expect(subject.valid_name?).to eq(true)
     end
   end
   describe '#valid_sign?' do
-  let (:name) {'elisha'}
+    let(:name) { 'elisha' }
     it 'it returns true if the sign is correct' do
       subject.sign = 'O'
       expect(subject.valid_sign?).to eq(true)
@@ -20,22 +19,18 @@ describe Player do
   end
 end
 
-my_game = Game.new('e', 'w')
-my_game.board = ['X ', 'X ', 'O ', 'O ', 'O ', 'O ', ' ', ' ', ' ']
 describe Game do
+  subject { Game.new('elisha', 'wandji') }
   it 'Returns true for valid moves' do
-    expect(my_game.valid_move?(1)).to eq(false)
+    subject.board = ['X ', 'X ', 'O ', 'O ', 'O ', 'O ', ' ', ' ', ' ']
+    expect(subject.valid_move?(1)).to eq(false)
   end
   it 'returns true when the win combination is found' do
-    expect(my_game.won?).to eq([3, 4, 5])
+    subject.board = ['X ', 'X ', 'O ', 'O ', 'O ', 'O ', ' ', ' ', ' ']
+    expect(subject.won?).to eq([3, 4, 5])
   end
   it 'returns true if position is taken' do
-    expect(my_game.position_taken?(2)).to eq(true)
+    subject.board = ['X ', 'X ', 'O ', 'O ', 'O ', 'O ', ' ', ' ', ' ']
+    expect(subject.position_taken?(2)).to eq(true)
   end
 end
-
-# describe game = Game.new do
-#   it 'returns true when the win combination is found' do
-#   expect(
-#   end
-# end
