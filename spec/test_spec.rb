49367b957a4268f618f640295ac6f37f606ca5fa
elisha2kyakpo1 <kyakopoe@gmail.com>
2020-12-09 10:45:16 +0200

    test a board class

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -13,7 +13,17 @@ end
 
 my_game = Game.new('e', 'w')
 describe Game do
+  let (:my_game.board) { ['X ', 'X ', 'X ', ' ', ' ', ' ', ' ', ' ', ' '] }
   it 'Returns true for valid moves' do
-    expect(my_game.valid_move?(1)).to_not eq(false)
+    expect(my_game.valid_move?(1)).to eq(false)
+  end
+  it 'returns true when the win combination is found' do
+    expect(my_game.won?).to eq([3, 4, 5])
   end
 end
+
+# describe game = Game.new do
+#   it 'returns true when the win combination is found' do
+#   expect(
+#   end
+# end
